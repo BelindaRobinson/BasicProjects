d3aad3ecb4cec4b75ec7fab28740d35e88c48164
Belinda Robinson <9927721@student.toiohomai.ac.nz>
2018-02-27 16:20:12 +1300

    Basic's 1 and 2

---
+++ Basic2/Basic2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 74;
+            var b = 36;
+
+            Console.WriteLine($"{(a + b)}");
+        }
+    }
+}
