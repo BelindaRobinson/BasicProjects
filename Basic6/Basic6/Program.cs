f0d9056460e3dc438cb6c431ac8b7848b11459c9
Belinda Robinson <9927721@bcs.net.nz>
2018-02-27 22:38:15 +1300

    not finished

---
+++ Basic6/Basic6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the radius of a circke to found the area and perimeter");
+            const double pi = 3.14159;
+            var input = double.Parse(Console.ReadLine());
+
+            var radius = Math.Sqrt(input);
+            var area = Math.Round(pi * radius, 4);
+
+            Console.WriteLine($"{area}");
+        }
+    }
+}
