6d78535ce8a02f4d00b6541370ef937bd9d5caf9
Belinda Robinson <9927721@student.toiohomai.ac.nz>
2018-02-28 17:02:08 +1300

    Up to date

---
+++ Basic9/Basic9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input1 = double.Parse(Console.ReadLine());
+            var input2 = double.Parse(Console.ReadLine());
+            var input3 = double.Parse(Console.ReadLine());
+
+            if (input1 > input2)
+            {
+                
+            }
+                        
+        }
+    }
+}
